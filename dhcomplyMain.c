42bfcf2c098f96bb119dd1a6cc7ff3af67647f98
Benjamin Thomas <brt1030@usnh.edu>
2025-05-31 22:58:13 -0400

    trying something

--- dhcomplyMain.c
+++ dhcomplyMain.c
@@ -1,5 +1,36 @@
 #include "dhcomplyFunctions.h"
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <net/if.h>
+#include <sys/socket.h>
+
+int main() {
+    int sock = socket(AF_INET6, SOCK_DGRAM, IPPROTO_UDP);
+
+    struct sockaddr_in6 src = {0}, dest = {0};
+    src.sin6_family = AF_INET6;
+    src.sin6_port = htons(546);
+    src.sin6_addr = in6addr_any;
+    bind(sock, (struct sockaddr*)&src, sizeof(src));
+
+    dest.sin6_family = AF_INET6;
+    dest.sin6_port = htons(547);
+    inet_pton(AF_INET6, "ff02::1:2", &dest.sin6_addr);
+    dest.sin6_scope_id = if_nametoindex("eth0");  // replace with your interface
+
+    char msg[] = "DHCPv6 test";
+    sendto(sock, msg, sizeof(msg), 0, (struct sockaddr*)&dest, sizeof(dest));
+
+    return 0;
+}
+
+
+/*
+#include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
@@ -35,4 +66,4 @@ int main(int argc, char *argv[])
 
     close(sockfd);
     return 0;
-}
+}*/
