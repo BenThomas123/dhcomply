7599fd33c242fcac09cf2ba757ac0e773302d0a9
Benjamin Thomas <brt1030@usnh.edu>
2025-06-06 14:08:46 -0400

    new script to access wireshark

--- scripts/wireshark.sh
+++ scripts/wireshark.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo wireshark
+gnome-terminal -- bash -c "echo 'Launching Wireshark with sudo...'; sudo wireshark; echo 'Wireshark closed. Press any key to exit.'; read -n 1; exec bash"
