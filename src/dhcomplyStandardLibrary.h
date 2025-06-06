974fd05ef8f435df5e93e8dde45ab504da9a921f
Benjamin Thomas <brt1030@usnh.edu>
2025-06-05 23:11:24 -0400

    more changes

---
+++ src/dhcomplyStandardLibrary.h
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdbool.h>
+#include <string.h>
+#include <ctype.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <net/if.h>
+#include <time.h>
+
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int uint32_t;
+typedef unsigned long uint64_t;
+typedef unsigned long long uint128_t;
+
+void valid_file_pointer(FILE *);
+void valid_memory_allocation(void *);
+void valid_socket(int);
+void randomize();
+char *substring(const char *, size_t, size_t);
+char *trim(char *);
+void to_uppercase(char *);
